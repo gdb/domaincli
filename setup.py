aee2b446eb50a4073ca95bb2a0ecf4b248af4c8c
Greg Brockman <gdb@gregbrockman.com>
2011-06-12 23:42:19 -0700

    Use install_requires to install dependencies

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ path, script = os.path.split(sys.argv[0])
 os.chdir(os.path.abspath(path))
 
 setup(name='domaincli',
-      version='0.0.2',
+      version='0.0.3',
       description='Register domains from the command line',
       author='Greg Brockman',
       author_email='gdb@gregbrockman.com',
       url='https://github.com/thegdb/domaincli',
       scripts=['domaincli'],
-      requires=['stripe (>= 1.5.0)']
+      install_requires='stripe'
 )
